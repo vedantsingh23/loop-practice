d050baccc4f0ad524ca2e21994d9c1ee3ced70d7
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2024-02-05 16:45:51 -0700

    Add files via upload

---
+++ loop practice.js
@@ -0,0 +1,62 @@
+let sum = 0
+for (let i = 1; i <= 1000; i++) {
+    sum += i
+}
+console.log(sum)
+
+
+
+let summ = 0
+for (let i = 1; i <= 10; i++) {
+    summ += i
+}
+console.log(summ)
+
+
+
+let suum = 0
+for (let i = 93; i <= 845; i++) {
+    if(i%2 == 1){
+        suum += i
+    }
+}
+console.log(suum)
+
+
+
+let suumm = 0
+for (let i = 1; i <= 10; i++) {
+    if(i%2 == 1){
+        suumm += i
+    }
+}
+console.log(suumm)
+
+
+
+let ssum = 1
+for (let i = 1; i <= 400; i++) {
+    if(i%6 == 0){
+        ssum *= i
+    }
+}
+console.log(ssum)
+
+let num = 7
+for(let i = 2; i < num; i++) { 
+    if (num % i == 0) { 
+        console.log(num + " is not prime");
+        return;
+    }
+}
+console.log("is prime");
+
+for(i = 1; i<= 100; i++){
+    
+}
+
+
+
+
+
+
