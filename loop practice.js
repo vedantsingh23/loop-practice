2c64c5f46640d9df75a4084ca011600ffa0cb574
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2024-02-05 22:00:17 -0700

    Update loop practice.js

--- loop practice.js
+++ loop practice.js
@@ -55,6 +55,83 @@ for(i = 1; i<= 100; i++){
     
 }
 
+let primes = [];
+
+for (i = 2; i <= 100; i++) {
+    let isPrime = true;
+    for (let j = 2; j <= Math.sqrt(num); j++) {
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        primes.push(i);
+    }
+}
+console.log(primes)
+
+let n = 4
+if(n === 0 || n === 1){
+    return 1
+} else {
+    let factorial = 1;
+    for (let i = 2; i <= n; i++) {
+        factorial *= i;
+    }
+    console.log(factorial);
+}
+
+
+let n1 = 20
+let n2 = 8
+let smallerNumber = Math.min(n1, n2);
+let gcf = 1;
+
+for (let i = 2; i <= smallerNumber; i++) {
+if (n1 % i === 0 && n2 % i === 0) {
+    gcf = i;
+}
+}
+
+console.log(gcf)
+
+x = 5
+for (let i = 1; i <= x; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '*';
+    }
+    console.log(row);
+}
+
+for (let i = x - 1; i >= 1; i--) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '*';
+    }
+    console.log(row);
+}
+
+n = 8
+let fib = [];
+fib[0] = 0;
+fib[1] = 1;
+for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+}
+console.log(fib);
+
+n = 4
+let square = '';
+for (let i = 0; i <= n; i++) {
+    let row = '';
+    for (let j = 0; j <= n; j++) {
+        row += Math.abs(i - j) + ' ';
+    }
+    square += row.trim() + '\n';
+}
+console.log(square)
 
 
 
